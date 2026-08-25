145fb0e5bc25dc111c4c647eb9900778f073ae46
VoltageTech011 <voltagelordtech@gmail.com>
2026-08-25 17:48:15 +0100

    Add conversionCategories for various measurement types

---
+++ js/converter/conversion.js
@@ -0,0 +1,11 @@
+// js/converter/conversion.js
+export const conversionCategories = {
+  'Length': ['meter','kilometer','centimeter','millimeter','mile','yard','foot','inch'],
+  'Weight': ['gram','kilogram','ton','pound','ounce'],
+  'Temperature': ['Celsius','Fahrenheit','Kelvin'],
+  'Area': ['square meter','square kilometer','square foot','square mile','hectare'],
+  'Volume': ['liter','milliliter','gallon','cubic meter'],
+  'Time': ['second','minute','hour','day','week','month','year'],
+  'Speed': ['m/s','km/h','mph','knot'],
+  'Data': ['byte','KB','MB','GB','TB']
+};
