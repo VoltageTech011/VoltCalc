cf9b4b1cec64db23c5ff073aee328eddae386cca
VoltageTech011 <voltagelordtech@gmail.com>
2026-08-25 17:51:58 +0100

    Add showToast function for displaying messages

---
+++ js/ui/toast.js
@@ -0,0 +1,9 @@
+// js/ui/toast.js
+export function showToast(message) {
+  const container = document.getElementById('toastContainer');
+  const toast = document.createElement('div');
+  toast.className = 'toast';
+  toast.textContent = message;
+  container.appendChild(toast);
+  setTimeout(() => toast.remove(), 2000);
+}
